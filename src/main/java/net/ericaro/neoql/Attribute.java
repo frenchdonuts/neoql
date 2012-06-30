ea44063fa43944c386a8447272d4da9430be3fbe
Eric <eric@ericaro.net>
2012-06-30 14:13:58 +0200

    documenting a little bit

--- src/main/java/net/ericaro/neoql/Attribute.java
+++ src/main/java/net/ericaro/neoql/Attribute.java
@@ -1,7 +1,9 @@
 package net.ericaro.neoql;
 
 
-/** Interface to provide full access to a Table class attributes.
+/** Provides full access to a Table Class attributes.
+ * ContentTable require to have full access to a TableClass attribute, even if the object's public interface 
+ * must forbid any editing.
  * 
  * 
  * @author eric
@@ -11,18 +13,24 @@ package net.ericaro.neoql;
  */
 public interface Attribute<T,V> {
 
-	
-	public Class<V> getType();
-	/** return the attribute's value for object
+	/**
 	 * 
-	 * @param object
-	 * @return
+	 * @return the attribute actual type.
 	 */
-	public V get(T object);
-	/** set the attributes's value for object 
+	public Class<V> getType();
+	
+	/** return the attribute's value for a row
 	 * 
-	 * @param object
+	 * @param row
+	 * @return the actual value for the row's attribute.
+	 */
+	public V get(T row);
+	
+	/** set the attributes's value for the row 
+	 * 
+	 * @param row
 	 * @param value
 	 */
-	public void set(T object, V value);
+	public void set(T row, V value);
+	
 }
